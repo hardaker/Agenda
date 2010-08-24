87ee98c48aadf72213ea726f889891df8d5b9cd8
Wes Hardaker <opensource@hardakers.net>
2010-08-24 16:00:54 -0700

    started working on agenda topic class

--- AgendaTopic.cpp
+++ AgendaTopic.cpp
@@ -4,3 +4,21 @@ AgendaTopic::AgendaTopic(QObject *parent) :
     QObject(parent)
 {
 }
+
+AgendaTopic::AgendaTopic(const QString &topicName, const QTime &timeNeeded, const QTime &timeSpent, QObject *parent)
+    : m_topicName(topicName), m_timeNeeded(timeNeeded), m_timeSpent(timeSpent)
+{
+}
+
+const QString &AgendaTopic::topicName() const {
+    return m_topicName;
+}
+
+const QTime &AgendaTopic::timeNeeded() const {
+    return m_timeNeeded;
+}
+
+const QTime &AgendaTopic::timeSpent() const {
+    return m_timeSpent;
+}
+
--- AgendaTopic.h
+++ AgendaTopic.h
@@ -2,17 +2,28 @@
 #define AGENDATOPIC_H
 
 #include <QObject>
+#include <QtCore/QString>
+#include <QtCore/QTime>
 
 class AgendaTopic : public QObject
 {
     Q_OBJECT
 public:
     explicit AgendaTopic(QObject *parent = 0);
+    AgendaTopic(const QString &topicName, const QTime &timeNeeded, const QTime &timeSpent = QTime(0,0), QObject *parent = 0);
+
+    const QString &topicName() const;
+    const QTime   &timeNeeded() const;
+    const QTime   &timeSpent() const;
 
 signals:
 
 public slots:
 
+private:
+    QString     m_topicName;
+    QTime       m_timeNeeded;
+    QTime       m_timeSpent;
 };
 
 #endif // AGENDATOPIC_H
