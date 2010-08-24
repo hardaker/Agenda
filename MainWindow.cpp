4711fb57a2c15e912679ebf5d29c0226ed376d0c
Wes Hardaker <opensource@hardakers.net>
2010-08-24 15:52:10 -0700

    starting setup

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -6,9 +6,19 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    mainWindowSetup(ui);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::mainWindowSetup(Ui::MainWindow *ui) {
+    ui->nextTopic->setText("foo");
+    ui->topic->setText("here");
+
+    ui->totalTime->setText("4:05 / 12:00");
+    ui->timeLeft->setText("0:05 / 5:00");
+
 }
